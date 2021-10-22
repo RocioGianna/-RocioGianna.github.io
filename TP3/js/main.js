a7d2be0d28583e8831a1a9927e6c93a70f148350
Juan Mauro <jmauro492@gmail.com>
2021-10-22 20:08:35 -0300

    Corrección initGame main.js

--- TP3/js/main.js
+++ TP3/js/main.js
@@ -5,10 +5,6 @@ let Captain = new Personaje ("Capitán América", 117, 212, 0, 0, 0);
 // cuando se generen las gemas, el método va a realizar que: de de forma aleatoria con un número de 0 a 5 y trae el objeto de esa posición
 
 let juego = new Juego(Captain, "a");
-juego.iniciarJuego();
+juego.initGame();
 // Crear personaje
 // Crear obstáculos
-
-// let gemass = document.getElementById("gemas");
-
-// gemass.style.backgroundImage = "url('img_tree.png')";
