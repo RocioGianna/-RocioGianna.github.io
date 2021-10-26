8a607b1b0fcc98ba4700850f5dc0235f321cdf0b
Juanemauro <jmauro492@gmail.com>
2021-10-26 12:47:04 -0300

    Pantalla inicio implementada

--- TP3/js/main.js
+++ TP3/js/main.js
@@ -1,6 +1,25 @@
-// Crear elementos del juego
-let Captain = new Personaje("Capitán America");
+// Obtener id de la galería para trabajar con el cambio de imágenes
+let imageGallery = document.getElementById("imageGallery");
+let idImage = 1;
+// window.addEventListener('load', (event) =>{
+//  console.log('carga audio');
+//  playBackgroundSong();   
+// })
 
-let juego = new Juego(Captain);
-juego.initGame();
+// Cambio de imágenes en la galería de la pantalla inicial
+function playGallery(){
+    if (idImage > 3){
+        idImage = 0;
+    }
+    // @ts-ignore
+    imageGallery.src = "img/image-gallery-" + idImage + ".jpg";    
+    idImage++;
+    setTimeout(playGallery, 1000);
+}
+
+// function playBackgroundSong(){
+//     let audioBackground = new Audio("audio/background_index.mp3");
+//     audioBackground.play();
+// }
+playGallery();
 
---
+++ TP3/js/mainGame.js
@@ -0,0 +1,6 @@
+// Crear elementos del juego
+let Captain = new Personaje("Capitán America");
+
+let juego = new Juego(Captain);
+juego.initGame();
+
